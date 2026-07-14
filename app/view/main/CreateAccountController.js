2aeea3a77b256cfca852baa8a6141b58cefec25e
Willy Aguirre <marti1125@gmail.com>
2026-07-14 15:33:04 -0500

    adding project

---
+++ app/view/main/CreateAccountController.js
@@ -0,0 +1,39 @@
+Ext.define('MyApp.view.main.CreateAccountController', {
+    extend: 'Ext.app.ViewController',
+    alias: 'controller.create-account',
+
+    onRegister: function() {
+        var me = this,
+            errorCmp = me.lookup('formRegisterFailure'),
+            fields,
+            form = me.lookup('formCreateAccount').getForm(),
+            errors = [],
+            data = {
+                errors: errors
+            };
+
+        if (form.isValid()) {
+            console.log(form.getValues());
+            Ext.Msg.alert('Registration Successful', 'You have successfully registered!');
+        }
+        else {
+            Ext.Msg.alert('Registration Failure', 'Please check for form errors and retry.');
+            fields = form.getFields();
+
+            fields.each(function(field) {
+                var error;
+
+                if (!field.validate() && (error = field.getErrors())) {
+                    errors.push({
+                        errors: error,
+                        name: field.getFieldLabel()
+                    });
+                }
+            });
+        }
+
+        if (errorCmp) {
+            errorCmp.setData(data);
+        }
+    }
+});
